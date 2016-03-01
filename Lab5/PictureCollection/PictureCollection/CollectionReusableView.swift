705244d26c1e64c6175c72d590be289eb4f21daa
Micro1435 <michael.montella@colorado.edu>
2016-03-01 09:06:00 -0700

    Lab 6

---
+++ Lab5/PictureCollection/PictureCollection/CollectionReusableView.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionReusableView.swift
+//  PictureCollection
+//
+//  Created by Michael Montella on 2/16/16.
+//  Copyright © 2016 Michael Montella. All rights reserved.
+//
+
+import UIKit
+
+class CollectionReusableView: UICollectionReusableView {
+        
+}
---
+++ Lab5/PictureCollection/PictureCollection/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  PictureCollection
+//
+//  Created by Michael Montella on 2/16/16.
+//  Copyright © 2016 Michael Montella. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
